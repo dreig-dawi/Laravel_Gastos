21f7a4345c155aa61eb81fb74428992b871ab101
dreig-dawi <dreigbuades@cifpfbmoll.eu>
2025-01-29 17:26:14 +0100

    Added: Updating expense methods

--- Expenses/app/Http/Controllers/ExpenseController.php
+++ Expenses/app/Http/Controllers/ExpenseController.php
@@ -6,74 +6,84 @@ use App\Models\Expense;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
 
-class ExpenseController extends Controller
-{
+class ExpenseController extends Controller {
+
     /**
      * Display a listing of the resource.
      */
-    public function index(): View
-    {
+    public function index(): View {
         return view(
             'expenses.index',
             [
-                'expenses' => Expense::with('user')->latest()->get(),
-            ]
+                'expenses' => Expense::with( 'user' )->latest()->get(),
+            ],
         );
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse
-    {
-        $validated = $request->validate([
+    public function store( Request $request ): RedirectResponse {
+        $validated = $request->validate( [
             'message' => 'required|string|max:255',
-        ]);
+        ] );
 
-        $request->user()->expenses()->create($validated);
+        $request->user()->expenses()->create( $validated );
 
-        return redirect(route('expenses.index'));
+        return redirect( route( 'expenses.index' ) );
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Expense $gasto)
-    {
+    public function show( Expense $expense ) {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Expense $gasto)
-    {
-        //
+    public function edit( Expense $expense ): View {
+        Gate::authorize( 'update', $expense );
+
+        return view(
+            'expenses.edit',
+            [
+                'expense' => $expense,
+            ],
+        );
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Expense $gasto)
-    {
-        //
+    public function update( Request $request, Expense $expense ): RedirectResponse {
+        Gate::authorize( 'update', $expense );
+
+        $validated = $request->validate( [
+            'message' => 'required|string|max:255',
+        ] );
+
+        $expense->update( $validated );
+
+        return redirect( route( 'expenses.index' ) );
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Expense $gasto)
-    {
+    public function destroy( Expense $expense ) {
         //
     }
+
 }
--- Expenses/routes/web.php
+++ Expenses/routes/web.php
@@ -19,7 +19,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('expenses', ExpenseController::class)
-    ->only(['index', 'store'])
+    ->only(['index', 'store', 'edit', 'update'])
     ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
